972bb6be8a622f1d8223606de0ca70f435176d40
Samuel Liebert <Samuelliebert@gmail.com>
2022-07-19 17:21:05 -0500

    added authentication along with navbar that uses authentication set up context for user and username currently active linked to homepage in navbar along with displaying username and profile image

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { useUserData } from '../lib/hooks';
+import { UserContext } from '../lib/context'
 import '../styles/globals.css'
+import NavBar from '../components/navbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  const userData = useUserData();
+
+  return (
+    <UserContext.Provider value={userData}>
+      <NavBar/>
+      <Component {...pageProps} />
+    </UserContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
