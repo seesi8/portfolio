2a9a43bde408460475714eaec37415ebfc094ac5
Samuel Liebert <Samuelliebert@gmail.com>
2022-07-30 10:45:35 -0500

    added timeline data

--- components/timeline.jsx
+++ components/timeline.jsx
@@ -1,4 +1,4 @@
-import styles from './timeline.module.css'
+import styles from '.timeline.module.css'
 
 export default function timeline({ }) {
     return (
@@ -9,19 +9,24 @@ export default function timeline({ }) {
                     <div className={styles.line}></div>
                 </div>
                 <li className={styles.item}>
-                    <h1 className={styles.itemTitle}>Demo Poj</h1>
-                    <p className={styles.itemDate}>This is date for a thing</p>
-                    <h2 className={styles.itemBody}>this is a breif discription of a even that happened on the couse of my life whihc is really cools</h2>
+                    <h1 className={styles.itemTitle}>I made this website</h1>
+                    <p className={styles.itemDate}>8/18/22</p>
+                    <h2 className={styles.itemBody}>In my cource of wanting to become a better programmer I wanted to learn a js framework. Because of it's popularity i choose react. I did some other projects with it before staritng this page</h2>
                 </li>
                 <li className={styles.item}>
-                    <h1 className={styles.itemTitle}>Demo Poj</h1>
-                    <p className={styles.itemDate}>This is date for a thing</p>
-                    <h2 className={styles.itemBody}>this is a breif discription of a even that happened on the couse of my life whihc is really cools</h2>
+                    <h1 className={styles.itemTitle}>Quarentine started</h1>
+                    <p className={styles.itemDate}>3/13/2022</p>
+                    <h2 className={styles.itemBody}>when covid started I continued my coding and worked on more advanced projects using python and eventualy into js and c#</h2>
+                </li>
+                <li className={styles.item}>
+                    <h1 className={styles.itemTitle}>I made my fist website</h1>
+                    <p className={styles.itemDate}>Early 2017</p>
+                    <h2 className={styles.itemBody}>I was in fourth grade and I was amazed by the idea of having a website. I wanted to create my own websites for me and my friends to use. A couple tutorials and reading the documentation later it was made.</h2>
                 </li>
                 <li className={styles.item}>
                     <h1 className={styles.itemTitle}>I did my first coding project</h1>
                     <p className={styles.itemDate}>Sometime in 2014</p>
-                    <h2 className={styles.itemBody}>Yound me had wanted my own live camera. My parents suggested that instead of buying it </h2>
+                    <h2 className={styles.itemBody}>Yound me had wanted my own live camera. My parents suggested that instead of buying it. I created a web server using a raspberry pi and created a live camera using a rapserry pi 0 </h2>
                 </li>
                 <li className={styles.item}>
                     <h1 className={styles.itemTitle}>I Was Born</h1>
@@ -29,7 +34,7 @@ export default function timeline({ }) {
                     <h2 className={styles.itemBody}>I was born in 2008 in Chicago, IL.</h2>
                 </li>
                 <div className="cirlceContainer">
-                <div className="circle"></div>
+                    <div className="circle"></div>
                 </div>
             </ul>
         </>
