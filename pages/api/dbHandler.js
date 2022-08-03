54b38841dfd8bd80bf6ff52f926ea18db4e67ce1
Sam Liebert <samliebert@Sams-MacBook-Pro.local>
2022-08-03 11:59:06 +0200

    fixed api request

--- pages/api/dbHandler.js
+++ pages/api/dbHandler.js
@@ -6,14 +6,19 @@ import { v4 as uuidv4 } from 'uuid';
 export default async function dbHandler(req, res) {
   const query = req.query;
   const { domain, password } = query;
+  console.log(req.body)
+  console.log(domain)
+  const params = {
+    domain: domain,
+    body: req.body
+  }
   if (password.slice(1) / password.charAt(0) == 940358) {
     const docRef = await setDoc(doc(firestore, "knowledge", uuidv4()), {
-        domain: domain,
-        body: req.body.stuff
+      ...params
     });
     res.status(200).json({ status: true })
   }
-  else{
+  else {
     res.status(200).json({ status: false })
   }
 }
