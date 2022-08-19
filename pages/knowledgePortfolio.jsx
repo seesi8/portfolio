f6ba5802d3ac5e86fb214be86810c198f28ed6d7
Sam Liebert <samliebert@Sams-MacBook-Pro.local>
2022-08-18 20:53:58 -0400

    fixed escaped charictor error

--- pages/knowledgePortfolio.jsx
+++ pages/knowledgePortfolio.jsx
@@ -3,7 +3,6 @@ import { v4 as uuidv4 } from 'uuid';
 import Knowledge from '../components/knowledge';
 import { collection, query, where, getDocs, orderBy, doc, getDoc, limit } from "firebase/firestore";
 import { firestore } from '../lib/firebase';
-import Head from 'next/head';
 
 export async function getServerSideProps({ }) {
 
@@ -23,14 +22,8 @@ export async function getServerSideProps({ }) {
 
 export default function knowledgePortfolio({ items }) {
     return (
-        <>
-            <Head>
-                <title>Samuel Liebert's Knolage Portfoloio</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            </Head>
-            <main className={styles.knowlage}>
-                {items ? items.map((item) => <><Knowledge theKnowledge={item} key={uuidv4()} /></>) : null}
-            </main>
-        </>
+        <main className={styles.knowlage}>
+            {items ? items.map((item) => <><Knowledge theKnowledge={item} key={uuidv4()} /></>) : null}
+        </main>
     )
 }
